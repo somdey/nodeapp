b41072de4b0a52229deb0ffb6de3eae97db720e6
Somenath Dey <somnath2prl@gmail.com>
2017-09-22 00:37:18 +0530

    CRUD Operation using Mongoclient

--- server/api/models/contacts.js
+++ server/api/models/contacts.js
@@ -1,14 +1,36 @@
 'use strict';
 const CONTACTS_COLLECTION = 'contacts'; 
 
+var ObjectID = require('mongodb').ObjectID;
+
 exports.fetchAllContacts = function (db, callback) {
     db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
         return callback(err, docs);
     });
 }
-exports.createContacts = function(db, newContact, callback) {
+
+exports.createContact = function(db, newContact, callback) {
     db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, docs) {  
-     return callback(err, docs);
+        return callback(err, docs.ops[0]);
+    });
+}
+
+exports.findById = function(db, id, callback) {
+    db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(id) }, function(err, docs) {  
+        return callback(err, docs);
+    });
+}
+
+exports.updateContact = function(db, id, updateDoc, callback) {
+    db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(id)}, updateDoc, function(err, docs) {
+        updateDoc._id = id;
+        return callback(err, updateDoc);
+    });
+}
+
+exports.deleteContact = function(db, id, callback) {
+    db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(id)}, function(err, result) { 
+       return callback(err, id);
     });
 }
 
--- server/api/routes/contacts.js
+++ server/api/routes/contacts.js
@@ -3,5 +3,8 @@ var Contacts = require('../controllers/contacts');
 
 module.exports = function (app) {
     app.get("/api/contacts", Contacts.listContacts);
-    app.post("/api/contacts", Contacts.CreateContacts);
+    app.post("/api/contacts", Contacts.createContact);
+    app.get("/api/contacts/:id", Contacts.findOneContact);
+    app.put("/api/contacts/:id", Contacts.updateContact);
+    app.delete("/api/contacts/:id", Contacts.deleteContact);
 }
