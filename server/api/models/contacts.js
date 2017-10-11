fcfec86f4b9559463e95084bc4df025596751ff8
Somenath Dey <somnath2prl@gmail.com>
2017-10-12 01:55:56 +0530

    Example use bookshelf

--- server/api/models/contacts.js
+++ server/api/models/contacts.js
@@ -1,48 +1,53 @@
 'use strict';
 const CONTACTS_COLLECTION = 'contacts'; 
 
-var ObjectID = require('mongodb').ObjectID;
+var bookshelf = require('../../db');
+// Pass an initialized bookshelf instance
+var ModelBase = require('bookshelf-modelbase')(bookshelf);
 
-var database = require('../../db');
- 
-exports.fetchAllContacts = function (callback) {
-  database.getDb(function(err, db) {
-    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
-      return callback(err, docs);
-    });
-  });
-}
+var Contacts = ModelBase.extend({
+  tableName: CONTACTS_COLLECTION
+});
+
+// exports.fetchAllContacts = function (callback) {
+//   database.getDb(function(err, db) {
+//     db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+//       return callback(err, docs);
+//     });
+//   });
+// }
 
 exports.createContact = function(newContact, callback) {
-  database.getDb(function(err, db) {
-    db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, docs) {  
-      return callback(err, docs.ops[0]);
-    });
-  });
+  Contacts.create(newContact)
+  .then(function (err) {
+    callback(err);
+  }, function(result){
+    callback(null, result);
+  })
 }
 
-exports.findById = function(id, callback) {
-  database.getDb(function(err, db) {
-    db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(id) }, function(err, docs) {  
-      return callback(err, docs);
-    });
- });
-}
+// exports.findById = function(id, callback) {
+//   database.getDb(function(err, db) {
+//     db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(id) }, function(err, docs) {  
+//       return callback(err, docs);
+//     });
+//  });
+// }
 
-exports.updateContact = function(id, updateDoc, callback) {
-  database.getDb(function(err, db) {
-    db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(id)}, updateDoc, function(err, docs) {
-      updateDoc._id = id;
-      return callback(err, updateDoc);
-    });
-  });
-}
+// exports.updateContact = function(id, updateDoc, callback) {
+//   database.getDb(function(err, db) {
+//     db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(id)}, updateDoc, function(err, docs) {
+//       updateDoc._id = id;
+//       return callback(err, updateDoc);
+//     });
+//   });
+// }
 
-exports.deleteContact = function(id, callback) {
-  database.getDb(function(err, db) {
-    db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(id)}, function(err, result) { 
-      return callback(err, id);
-    });
-  });
-}
+// exports.deleteContact = function(id, callback) {
+//   database.getDb(function(err, db) {
+//     db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(id)}, function(err, result) { 
+//       return callback(err, id);
+//     });
+//   });
+// }
 
